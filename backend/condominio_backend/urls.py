d75085dfd28e1c94c0929d9b7fcd6d553f79b859
Judith Sarai Garces <saray2006.outlook@gmail.com>
2025-11-10 17:28:37 -0500

    Update urls.py

--- backend/condominio_backend/urls.py
+++ backend/condominio_backend/urls.py
@@ -1,9 +1,12 @@
 """
 URL configuration for condominio_backend project.
 """
-from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse  # 👈 Import necesario
+from rest_framework.authtoken.views import obtain_auth_token  # 👈 Import del login token
+from api.views import dashboard_stats  # 👈 Import de la vista que creaste (ajústalo si está en otra parte)
+
 
 urlpatterns = [
     path('', lambda request: JsonResponse({"message": "API Condominio activa"})),
