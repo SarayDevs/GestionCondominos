c18893785df6cbf62dd74615cafb18e38438b9b1
Judith Sarai Garces <saray2006.outlook@gmail.com>
2025-11-10 17:18:31 -0500

    Update urls.py

--- backend/condominio_backend/urls.py
+++ backend/condominio_backend/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({"message": "API Condominio activa"})),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
