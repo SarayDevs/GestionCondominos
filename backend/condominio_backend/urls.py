7c85d5a5993e5645565ced98900d090309c78c50
Judith Sarai Garces <saray2006.outlook@gmail.com>
2025-11-10 17:26:34 -0500

    Update urls.py

--- backend/condominio_backend/urls.py
+++ backend/condominio_backend/urls.py
@@ -7,8 +7,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', lambda request: JsonResponse({"message": "API Condominio activa"})),
-    path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('auth/login/', obtain_auth_token, name='api_token_auth'),
+    path('dashboard/stats/', dashboard_stats, name='dashboard_stats'),
+    path('admin/', admin.site.urls),
 ]
 
 
